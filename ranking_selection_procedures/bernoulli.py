bdde0b128e959a7faa23f1dfe126a282349b5a62
mahsoo1172 <95515986+mahsoo1172@users.noreply.github.com>
2025-04-08 22:09:32 -0400

    initial commit for skeleton of folder structure

---
+++ ranking_selection_procedures/bernoulli.py
@@ -0,0 +1,16 @@
+def example_format():
+    """
+    Single-stage selection procedure to select the normal distribution with the highest mean.
+    Based on Bechhofer's 1954 paper: "A Single-Sample Multiple Decision Procedure for Ranking
+    Means of Normal Populations with known Variances"
+
+    Assumptions:
+
+    Parameters:
+    parameter1 (type): Description of the first parameter.
+    parameter2 (type): Description of the second parameter.
+
+    Returns:
+    return_type: Description of the return value.
+    """
+    return
---
+++ ranking_selection_procedures/normal.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def single_stage():
+    """
+    Single-stage selection procedure to select the normal distribution with the highest mean.
+    Based on Bechofer's 1954 paper: "A Single-Sample Multiple Decision Procedure for Ranking
+    Means of Normal Populations with known Variances"
+
+    Assumptions:
+
+    Parameters:
+    parameter1 (type): Description of the first parameter.
+    parameter2 (type): Description of the second parameter.
+
+    Returns:
+    return_type: Description of the return value.
+    """
+    return
+
+def sequential():
+    """
+    Sequential selection procedure to select the normal distribution with the highest mean.
+    Based on Kim and Nelson's 2001 paper: "A Fully Sequential Procedure for Indifference-Zone
+    Selection in Simulation"
+
+    Assumptions:
+
+    Parameters:
+    parameter1 (type): Description of the first parameter.
+    parameter2 (type): Description of the second parameter.
+
+    Returns:
+    return_type: Description of the return value.
+    """
+    return
