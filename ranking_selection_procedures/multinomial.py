12f930fb2bf73426fcfcc2004a94678b687245db
Chuck Cao <40773046+cttlc2u@users.noreply.github.com>
2025-04-21 20:15:54 -0400

    Update multinomial.py
    Removed example function text

--- ranking_selection_procedures/multinomial.py
+++ ranking_selection_procedures/multinomial.py
@@ -1,20 +1,3 @@
-def example_format():
-    """
-    Single-stage selection procedure to select the normal distribution with the highest mean.
-    Based on Bechhofer's 1954 paper: "A Single-Sample Multiple Decision Procedure for Ranking
-    Means of Normal Populations with known Variances"
-
-    Assumptions:
-
-    Parameters:
-    parameter1 (type): Description of the first parameter.
-    parameter2 (type): Description of the second parameter.
-
-    Returns:
-    return_type: Description of the return value.
-    """
-    return
-
 import pickle
 import numpy as np
 from scipy.stats import multinomial
